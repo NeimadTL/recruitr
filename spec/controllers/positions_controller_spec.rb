e135c0a87d6f5d78e6985eb342cf0cd7fbe3ba05
neimad971 <damientalbot26@gmail.com>
2020-01-18 15:12:41 +0100

    fixes specs failures

--- spec/controllers/positions_controller_spec.rb
+++ spec/controllers/positions_controller_spec.rb
@@ -23,7 +23,7 @@ require 'rails_helper'
 # removed from Rails core in Rails 5, but can be added back in via the
 # `rails-controller-testing` gem.
 
-RSpec.describe PositionsController, type: :controller do
+RSpec.describe Hr::PositionsController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # Position. As you add validations to Position, be sure to
--- spec/routing/positions_routing_spec.rb
+++ spec/routing/positions_routing_spec.rb
@@ -1,38 +1,38 @@
 require "rails_helper"
 
-RSpec.describe PositionsController, type: :routing do
+RSpec.describe Hr::PositionsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/positions").to route_to("positions#index")
+      expect(:get => "hr/positions").to route_to("hr/positions#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/positions/new").to route_to("positions#new")
+      expect(:get => "hr/positions/new").to route_to("hr/positions#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/positions/1").to route_to("positions#show", :id => "1")
+      expect(:get => "hr/positions/1").to route_to("hr/positions#show", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/positions/1/edit").to route_to("positions#edit", :id => "1")
+      expect(:get => "hr/positions/1/edit").to route_to("hr/positions#edit", :id => "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/positions").to route_to("positions#create")
+      expect(:post => "hr/positions").to route_to("hr/positions#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/positions/1").to route_to("positions#update", :id => "1")
+      expect(:put => "hr/positions/1").to route_to("hr/positions#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/positions/1").to route_to("positions#update", :id => "1")
+      expect(:patch => "hr/positions/1").to route_to("hr/positions#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/positions/1").to route_to("positions#destroy", :id => "1")
+      expect(:delete => "hr/positions/1").to route_to("hr/positions#destroy", :id => "1")
     end
   end
 end
