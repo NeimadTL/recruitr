b0a1f3f4f907a14665801bc1d5983c675de2b185
neimad971 <damientalbot26@gmail.com>
2020-01-21 21:43:05 +0100

    allows user to select skills when signing up
      - adds select field in on registration form
      - puts select field in a shared folder to use for hr/form and devise/registration/form

--- app/controllers/customised_registrations_controller.rb
+++ app/controllers/customised_registrations_controller.rb
@@ -8,7 +8,7 @@ class CustomisedRegistrationsController < Devise::RegistrationsController
 
     def sign_up_params
       params.require(resource_name).permit(:firstname, :lastname, :city,
-        :country, :email, :password, :password_confirmation)
+        :country, :email, :password, :password_confirmation, :skill_ids => [])
     end
 
 end
