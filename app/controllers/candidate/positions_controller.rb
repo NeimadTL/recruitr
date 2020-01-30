d0c50435a3331e82d956707d876665163287a5a9
neimad971 <damientalbot26@gmail.com>
2020-01-30 02:30:47 +0100

    secures the candidate side

--- app/controllers/candidate/positions_controller.rb
+++ app/controllers/candidate/positions_controller.rb
@@ -1,4 +1,6 @@
 class Candidate::PositionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_to_be_candidate
   before_action :set_position, only: [:show]
 
   # GET /positions
@@ -14,6 +16,12 @@ class Candidate::PositionsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_position
       @position = Position.find(params[:id])
+    end
+
+    def require_to_be_candidate
+      unless current_user.role.code == Role::CANDIDATE_ROLE_CODE
+        render :file => "#{Rails.root}/public/401", :layout => false, :status => :unauthorized
+      end
     end
 
 end
--- app/controllers/candidate/user_position_requests_controller.rb
+++ app/controllers/candidate/user_position_requests_controller.rb
@@ -1,4 +1,6 @@
 class Candidate::UserPositionRequestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_to_be_candidate
 
   def create
     position = Position.find(params[:position_id])
@@ -11,5 +13,13 @@ class Candidate::UserPositionRequestsController < ApplicationController
     current_user.withdraw(position)
     redirect_to candidate_positions_path, notice: 'Your application has been withdrawn.'
   end
+
+  private
+
+    def require_to_be_candidate
+      unless current_user.role.code == Role::CANDIDATE_ROLE_CODE
+        render :file => "#{Rails.root}/public/401", :layout => false, :status => :unauthorized
+      end
+    end
 
 end
